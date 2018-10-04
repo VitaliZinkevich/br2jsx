d6af1c25f6ddd6421c1c316689228f1bb20b5fb5
Vitalizinkevich <vitalizinkevich@gmail.com>
2018-10-04 16:53:10 +0300

    final edition

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Br2jsx from './components/Br2jsx'
 
 
-let text = 'получает <br/>ОДИН<br /> props, <br>содержащий <br/>многострочный текст,<br/> разбитый на строки тегами ,  или тображает этот<br /> многострочный текст внутри <br/>себя. '
+let text = 'получает <br/>ОДИН<br /> props, <br>содержащий <br/>многострочный текст,<br/> разбитый на строки<br> тегами <бр>,и отображает этот<br /> многострочный текст внутри <br/>себя. '
 
 class App extends Component {
   render() {
